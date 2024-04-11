9406b63532e8fe66bb68357fc3924499a5573bf7
Jaimey Frank <franjaim21@gmail.com>
2024-04-11 17:31:52 -0400

    little link fix

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Link from "next/link";
 
 const Profile = () => {
-    const userId = localStorage.getItem('userId');  // Retrieve user ID from local storage
+  const userId = localStorage.getItem("userId"); // Retrieve user ID from local storage
 
-    return (
-        <div>
-            <h1>User Profile</h1>
-            <p>Your user ID is: {userId}</p>
-            <a href="/">Back to Home</a>
-        </div>
-    );
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Your user ID is: {userId}</p>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
 };
 
 export default Profile;
