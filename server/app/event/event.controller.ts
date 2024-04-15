cde404bdf89dc4076d2f80e0ab9a2b9a8c103f2a
JykLouie <jykuang@bu.edu>
2024-04-15 19:41:01 -0400

    misc: update create event fields

--- server/app/event/event.controller.ts
+++ server/app/event/event.controller.ts
@@ -97,7 +97,7 @@ export const get_event_by_id = async (req: Request, res: Response) => {
 };
 
 export const create_event = async (req: Request, res: Response) => {
-  const { exp_time, description, qty, tags, location } = req.body;
+  const { exp_time, description, qty, tags } = req.body;
   try {
     const userId = req.body.user.id;
     const now = new Date().toISOString();
@@ -122,14 +122,6 @@ export const create_event = async (req: Request, res: Response) => {
         },
         createdAt: now,
         updatedAt: now,
-        location: {
-          create: {
-            Address: location.Address,
-            floor: location.floor,
-            room: location.room,
-            loc_note: location.loc_note,
-          },
-        },
         photos: {
           create: {
             photo: photoBase64,
