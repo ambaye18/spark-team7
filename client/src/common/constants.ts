e5a85da974dc97d58f97d391e571e3af54a9d039
JykLouie <jykuang@bu.edu>
2024-04-25 14:36:55 -0400

    url fix

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL: string =
   process.env.NODE_ENV === "production"
-    ? "https://marvelous-reprieve-production.up.railway.app/"
+    ? "https://marvelous-reprieve-production.up.railway.app"
     : "http://localhost:5005";
